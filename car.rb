6eb136179cbbc63fc39696ce3f4a33bd91fb60dd
Marissa <masimons66@gmail.com>
2021-02-15 17:44:23 -0600

    convert tabs to spaces

--- car.rb
+++ car.rb
@@ -2,22 +2,22 @@ require 'csv'
 require 'time'
 
 module Car
-	def self.average_daily_miles(csv_file_path, vehicle_id)
-		data = CSV.open(csv_file_path, headers: true).to_a
-		vehicle_data = data.filter { |vehicle| vehicle['vehicle_id'] == vehicle_id }
-		miles = vehicle_data.map { |vehicle| vehicle['odometer'].to_i }
-		total_miles = miles.max - miles.min
-		times = vehicle_data.map { |vehicle| Time.parse(vehicle['created_at']) }
-		range_in_days = (times.max - times.min)/86400.0
+  def self.average_daily_miles(csv_file_path, vehicle_id)
+    data = CSV.open(csv_file_path, headers: true).to_a
+    vehicle_data = data.filter { |vehicle| vehicle['vehicle_id'] == vehicle_id }
+    miles = vehicle_data.map { |vehicle| vehicle['odometer'].to_i }
+    total_miles = miles.max - miles.min
+    times = vehicle_data.map { |vehicle| Time.parse(vehicle['created_at']) }
+    range_in_days = (times.max - times.min)/86400.0
 
-		pp (total_miles/range_in_days).truncate(2)
-	end
+    pp (total_miles/range_in_days).truncate(2)
+  end
 
-	def self.charged_above(csv_file_path, percentage)
-		data = CSV.open(csv_file_path, headers: true).to_a
-		all_cars = data.map { |vehicle| vehicle['vehicle_id'] }.uniq
-		filtered = data.filter { |vehicle| vehicle['charge_reading'].to_f <= percentage.to_f }
+  def self.charged_above(csv_file_path, percentage)
+    data = CSV.open(csv_file_path, headers: true).to_a
+    all_cars = data.map { |vehicle| vehicle['vehicle_id'] }.uniq
+    filtered = data.filter { |vehicle| vehicle['charge_reading'].to_f <= percentage.to_f }
 
-		pp (all_cars - filtered.map { |car| car['vehicle_id'] }.uniq).count
-	end
+    pp (all_cars - filtered.map { |car| car['vehicle_id'] }.uniq).count
+  end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,48 +1,48 @@
 require_relative '../car'
 
 describe Car do
-	context '.average_daily_miles' do
-		let(:csv_file_path) { 'ev_data.csv' }
-		let(:vehicle_id) { 'cat-car' }
-		let(:result) { Car.average_daily_miles(csv_file_path, vehicle_id) }
+  context '.average_daily_miles' do
+    let(:csv_file_path) { 'ev_data.csv' }
+    let(:vehicle_id) { 'cat-car' }
+    let(:result) { Car.average_daily_miles(csv_file_path, vehicle_id) }
 
-		context 'vehicle_id is cat-car' do
-			let(:vehicle_id) { 'cat-car' }
+    context 'vehicle_id is cat-car' do
+      let(:vehicle_id) { 'cat-car' }
 
-			it 'should return correct miles' do
-				expect(result).to eq 10.32
-			end
-		end
+      it 'should return correct miles' do
+        expect(result).to eq 10.32
+      end
+    end
 
-		context 'vehicle_id is electric-suv' do
-			let(:vehicle_id) { 'electric-suv' }
+    context 'vehicle_id is electric-suv' do
+      let(:vehicle_id) { 'electric-suv' }
 
-			it 'should return correct miles' do
-				# 99/6.847974537037037 = 14.45
-				expect(result).to eq 14.45
-			end
-		end
-	end
+      it 'should return correct miles' do
+        # 99/6.847974537037037 = 14.45
+        expect(result).to eq 14.45
+      end
+    end
+  end
 
-	context '.charged_above' do
-		let(:csv_file_path) { 'ev_data.csv' }
-		let(:result) { Car.charged_above(csv_file_path, percentage) }
+  context '.charged_above' do
+    let(:csv_file_path) { 'ev_data.csv' }
+    let(:result) { Car.charged_above(csv_file_path, percentage) }
 
-		context 'percentage is 33%' do
-			let(:percentage) { '0.33' }
+    context 'percentage is 33%' do
+      let(:percentage) { '0.33' }
 
-			it 'should return correct number of cars' do
-				expect(result).to eq 2
-			end
-		end
+      it 'should return correct number of cars' do
+        expect(result).to eq 2
+      end
+    end
 
-		context 'percentage is 20%' do
-			let(:percentage) { '0.2' }
-			
-			it 'should return correct number of cars' do
-				# clown, hamster, electric
-				expect(result).to eq 3
-			end
-		end
-	end
+    context 'percentage is 20%' do
+      let(:percentage) { '0.2' }
+      
+      it 'should return correct number of cars' do
+        # clown, hamster, electric
+        expect(result).to eq 3
+      end
+    end
+  end
 end
