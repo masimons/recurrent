30d4d6478fa26c612fc3235016835b526109b483
Marissa <masimons66@gmail.com>
2021-02-15 17:15:56 -0600

    add some more specs

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -6,18 +6,43 @@ describe Car do
 		let(:vehicle_id) { 'cat-car' }
 		let(:result) { Car.average_daily_miles(csv_file_path, vehicle_id) }
 
-		it 'should return correct miles' do
-			expect(result).to eq 10.32
+		context 'vehicle_id is cat-car' do
+			let(:vehicle_id) { 'cat-car' }
+
+			it 'should return correct miles' do
+				expect(result).to eq 10.32
+			end
+		end
+
+		context 'vehicle_id is electric-suv' do
+			let(:vehicle_id) { 'electric-suv' }
+
+			it 'should return correct miles' do
+				# 99/6.847974537037037 = 14.45
+				expect(result).to eq 14.45
+			end
 		end
 	end
 
 	context '.charged_above' do
 		let(:csv_file_path) { 'ev_data.csv' }
-		let(:percentage) { '0.33' }
 		let(:result) { Car.charged_above(csv_file_path, percentage) }
 
-		it 'should return correct number of cars' do
-			expect(result).to eq 2
+		context 'percentage is 33%' do
+			let(:percentage) { '0.33' }
+
+			it 'should return correct number of cars' do
+				expect(result).to eq 2
+			end
+		end
+
+		context 'percentage is 20%' do
+			let(:percentage) { '0.2' }
+			
+			it 'should return correct number of cars' do
+				# clown, hamster, electric
+				expect(result).to eq 3
+			end
 		end
 	end
 end
