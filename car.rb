d5ad8f0eeef6599fae72adb4648b0b2cbe0ec341
Marissa <masimons66@gmail.com>
2021-02-15 17:23:36 -0600

    rename ambiguous variables

--- car.rb
+++ car.rb
@@ -3,11 +3,11 @@ require 'time'
 
 module Car
 	def self.average_daily_miles(csv_file_path, vehicle_id)
-		csv = CSV.open(csv_file_path, headers: true)
-		filtered = csv.filter { |row| row['vehicle_id'] == vehicle_id }
-		miles = filtered.map { |vehicle| vehicle['odometer'].to_i }
+		data = CSV.open(csv_file_path, headers: true).to_a
+		vehicle_data = data.filter { |vehicle| vehicle['vehicle_id'] == vehicle_id }
+		miles = vehicle_data.map { |vehicle| vehicle['odometer'].to_i }
 		total_miles = miles.max - miles.min
-		times = filtered.map { |vehicle| Time.parse(vehicle['created_at']) }
+		times = vehicle_data.map { |vehicle| Time.parse(vehicle['created_at']) }
 		range_in_days = (times.max - times.min)/86400.0
 
 		pp (total_miles/range_in_days).truncate(2)
@@ -15,8 +15,8 @@ module Car
 
 	def self.charged_above(csv_file_path, percentage)
 		data = CSV.open(csv_file_path, headers: true).to_a
-		all_cars = data.map { |row| row['vehicle_id'] }.uniq
-		filtered = data.filter { |row| row['charge_reading'].to_f <= percentage.to_f }
+		all_cars = data.map { |vehicle| vehicle['vehicle_id'] }.uniq
+		filtered = data.filter { |vehicle| vehicle['charge_reading'].to_f <= percentage.to_f }
 
 		pp (all_cars - filtered.map { |car| car['vehicle_id'] }.uniq).count
 	end
